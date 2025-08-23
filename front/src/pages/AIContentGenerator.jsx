39226012f42cfeb9fb1d983c017bbd0b964d800d
Yogesh <147805708+yogeshMern@users.noreply.github.com>
2025-08-23 13:45:45 +0530

    Update AIContentGenerator.jsx

--- front/src/pages/AIContentGenerator.jsx
+++ front/src/pages/AIContentGenerator.jsx
@@ -41,7 +41,7 @@ const AIContentGenerator = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:7000/api/v1/chat", {
+      const response = await fetch("https://ai-blog-writerandcode-generator-back.onrender.com/api/v1/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
